7f37ee3af3b420fccb388dd2b01fbac917d786d5
Lesly Fotso <146014435+Loys237@users.noreply.github.com>
2024-08-06 18:54:50 +0100

    Add files via upload
    A springboot-project containing a secured home and login page access

---
+++ springdemo/src/test/java/com/mySpringboot/springdemo/employmentdemo/EmploymentDemoApplication.java
@@ -0,0 +1,10 @@
+package com.mySpringboot.springdemo.employmentdemo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmploymentDemoApplication.class, args);
+    }
+}
---
+++ springdemo/src/test/java/com/mySpringboot/springdemo/employmentdemo/SecurityConfig.java
@@ -0,0 +1,35 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        RequestMatcher publicUrls = new AntPathRequestMatcher("/", "/home");
+
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(publicUrls).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .permitAll()
+                )
+                .logout(logout ->
+                        logout
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+}
